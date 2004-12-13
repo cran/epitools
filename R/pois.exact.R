3c2964b49aa6082b31cf716e35d5665489c4445b
Tomas Aragon <aragon@berkeley.edu>
2004-12-13 00:00:00 +0000

    version 0.3-3

--- R/pois.exact.R
+++ R/pois.exact.R
@@ -1,21 +1,23 @@
 "pois.exact" <-
-function(x, pt = 1, conf.level = 0.95){
-  xc <- cbind(x,conf.level)
+function(x, pt = 1, conf.level = .95){
+  ##updated 2004-11-29
+  xc <- cbind(x,conf.level,pt)
+  pt2 <- xc[,3]
   results <- matrix(NA,nrow(xc),6)
   f1 <- function(x,ans,alpha=alp) {ppois(x,ans)-alpha/2}
   f2 <- function(x,ans,alpha=alp) 1-ppois(x,ans)+dpois(x,ans)-alpha/2
   for(i in 1:nrow(xc)){
     alp <- 1-xc[i,2]
     interval <- c(0,xc[i,1]*5+4)
-    uci <- uniroot(f1,interval=interval,x=xc[i,1])$root/pt
+    uci <- uniroot(f1,interval=interval,x=xc[i,1])$root/pt2[i]
     if(xc[i,1]==0){
       lci <- 0
     } else {
-      lci <- uniroot(f2,interval=interval,x=xc[i,1])$root/pt
+      lci <- uniroot(f2,interval=interval,x=xc[i,1])$root/pt2[i]
     } 
-    results[i,] <- c(xc[i,1],pt,xc[i,1]/pt,xc[i,2],lci,uci) 
+    results[i,] <- c(xc[i,1],pt2[i],xc[i,1]/pt2[i],xc[i,2],lci,uci) 
   }
-  colnames <- c("x", "pt", "rate", "conf.level", "lci", "uci")
-  dimnames(results) <- list(NULL, colnames)
-  as.data.frame(results)
+  coln <- c("x","pt","rate","conf.level","lci","uci")
+  dimnames(results) <- list(NULL, coln)
+  results
 }
