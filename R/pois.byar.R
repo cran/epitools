d4ced5678e25b2684caf7fb81c1dff46b6e56171
Tomas Aragon <aragon@berkeley.edu>
2008-07-27 00:00:00 +0000

    version 0.5-0

--- R/pois.byar.R
+++ R/pois.byar.R
@@ -1,7 +1,7 @@
 "pois.byar" <-
   function(x, pt = 1, conf.level = 0.95) {
     Z <- qnorm(0.5*(1 + conf.level))
-    aprime <- x + 0.05
+    aprime <- x + 0.5
     Zinsert <- (Z/3)*sqrt(1/aprime)
     lower <- (aprime*(1-1/(9*aprime) - Zinsert)^3)/pt
     upper <- (aprime*(1-1/(9*aprime) + Zinsert)^3)/pt
